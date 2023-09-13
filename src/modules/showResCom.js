2bd15e73b823506f1f73201cf26d0d25e832576c
ometman <revomet@gmail.com>
2023-09-13 13:10:13 +0000

    Modify reservation popup heading

--- src/modules/showResCom.js
+++ src/modules/showResCom.js
@@ -3,7 +3,9 @@ import * as infoInterface from './rescomInterface.js';
 export const showReserveData = (resdata) => {
   resdata.forEach((res, resIndex) => {
     const resCount = resdata.length;
-    infoInterface.popupTitle.innerText = `Reservations (${resCount})`;
+    if (resCount >= 0) {
+      infoInterface.popupTitle.textContent = 'Reservations';
+    }
     const resList = document.createElement('li');
     resList.className = 'reserve-list-content';
     resList.innerHTML = `
