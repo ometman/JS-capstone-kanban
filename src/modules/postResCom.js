61e5036a09bad2f213392b0f740370f7e90e5334
Enoh <eno.col@gmail.com>
2023-09-14 11:06:09 +0100

    Edit the .js file

--- src/modules/postResCom.js
+++ src/modules/postResCom.js
@@ -1,30 +1,30 @@
-import * as resInterface from './rescomInterface.js';
+// import * as resInterface from './rescomInterface.js';
 
-// const url =
-('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/votSVyM8b966LHhFVfLV/reservations/');
+// // const url =
+// ('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/votSVyM8b966LHhFVfLV/reservations/');
 
-export const postResData = (imgId) => {
-  let count = 0;
-  const theForm = resInterface.resForm;
-  theForm.addEventListener('submit', async (ev) => {
-    ev.preventDefault();
-    count += 1;
-    if (count === imgId + 1) {
-      const itemid = String(imgId);
-      const username = resInterface.userInput.value;
-      const dateStart = resInterface.startDate.value;
-      const dateEnd = resInterface.endDate.value;
-      const resInput = {
-        itemid,
-        username,
-        dateStart,
-        dateEnd,
-      };
-      await fetch(`${url}`, {
-        method: 'POST',
-        body: JSON.stringify(resInput),
-      });
-      theForm.reset();
-    }
-  });
-};
+// export const postResData = (imgId) => {
+//   let count = 0;
+//   const theForm = resInterface.resForm;
+//   theForm.addEventListener('submit', async (ev) => {
+//     ev.preventDefault();
+//     count += 1;
+//     if (count === imgId + 1) {
+//       const itemid = String(imgId);
+//       const username = resInterface.userInput.value;
+//       const dateStart = resInterface.startDate.value;
+//       const dateEnd = resInterface.endDate.value;
+//       const resInput = {
+//         itemid,
+//         username,
+//         dateStart,
+//         dateEnd,
+//       };
+//       await fetch(`${url}`, {
+//         method: 'POST',
+//         body: JSON.stringify(resInput),
+//       });
+//       theForm.reset();
+//     }
+//   });
+// };
