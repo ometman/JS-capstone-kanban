982d45419ddac9e0b08f1c574f78a91d636e5aa6
Kenneth Adutwum <kwamayadu@gmail.com>
2023-09-16 12:04:34 -0700

    solving merging issue

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import './index.css';
 import getProducts from './modules/getProducts.js';
 import itemPopup from './modules/itemPopup.js';
 import closePopup from './modules/closePopUp.js';
-import Logo from './logo/logo.jpg';
+// import Logo from './logo/logo.jpg';
 import getResFormData, { getCommentsFormData } from './modules/getResFormData.js';
 import commentPopup from './modules/commentPopup.js';
-import getResFormData from './modules/getResFormData.js';
 import productCount from './modules/productCount.js';
 
 const store = document.querySelector('.counter');
-
 
 const logo = document.querySelector('.logo');
 logo.innerHTML = '<div class="oke">O K E</div>';
@@ -19,7 +17,7 @@ const itemsContainer = async () => {
   const theProducts = await getProducts();
   const productsList = document.getElementById('products-list');
   productsList.innerHTML = '';
-  
+
   const getAllProduts = async () => {
     for (let i = 0; i < theProducts.length; i += 1) {
       const itemId = i + 1;
@@ -30,7 +28,7 @@ const itemsContainer = async () => {
         <img class="store-items-img" src="${theProducts[i].image}"/>
         <div class="title-like"><h2 class="itemid">Item ${itemId} </h2>
         </div>
-        <button id =${i.itemId} comment-btn class="comment-btn">Comments</button>
+        <button id = comment-btn class="comment-btn">Comments</button>
         <button id ="reserve-btn" class="reserve-btn">Reservations</button>
         </li>`,
       );
